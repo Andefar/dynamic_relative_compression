2cb81c68834c914e5390b6e19aaff5345e3d9628
andefar40 <andreas.david.lauritzen@gmail.com>
2016-02-08 12:36:01 +0100

    operation class added

---
+++ v0/DynamicOperationsNaive.java
@@ -0,0 +1,15 @@
+/**
+ * Created by andl on 08/02/2016.
+ */
+public class DynamicOperationsNaive {
+
+    public DynamicOperationsNaive() {
+
+    }
+
+    public void access(int i) {}
+    public void replace(int i, char sub) {}
+    public void insert(int i, char c) {}
+    public void delete(int i) {}
+
+}
