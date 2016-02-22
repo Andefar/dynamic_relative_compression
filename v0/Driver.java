3659c97c96eaf924a552f8b83489717314b41f54
andefar40 <andreas.david.lauritzen@gmail.com>
2016-02-22 10:23:45 +0100

    print statements in driver to track progress

--- v0/Driver.java
+++ v0/Driver.java
@@ -31,46 +31,50 @@ public class Driver {
         String S, R;
 
         try {
+            System.out.println("Reading data from file..");
             S = f.readFromFileChar("/Users/Josefinetusindfryd/Desktop/dynamic_relative_compression/data/DNA_full");
             R = f.readFromFileChar("/Users/Josefinetusindfryd/Desktop/dynamic_relative_compression/data/DNA_R");
 
+            System.out.println("Building suffix tree..");
+            long suffixTreeCpuTimeStart = getCpuTime();
+            CompressorSuffix suf1 = new CompressorSuffix(R);
+            long suffixTreeCpuTimeEnd = getCpuTime() - suffixTreeCpuTimeStart;
+            long suffixTreeTimeStart = System.nanoTime();
+            CompressorSuffix suf2 = new CompressorSuffix(R);
+            long suffixTreeTimeEnd  = System.nanoTime() - suffixTreeTimeStart;
 
-        long suffixTreeCpuTimeStart = getCpuTime( );
-        CompressorSuffix suf1 = new CompressorSuffix(R);
-        long suffixTreeCpuTimeEnd = getCpuTime() - suffixTreeCpuTimeStart;
-        long suffixTreeTimeStart = System.nanoTime();
-        CompressorSuffix suf2 = new CompressorSuffix(R);
-        long suffixTreeTimeEnd  = System.nanoTime() - suffixTreeTimeStart;
-
-
-        ArrayList<Block> t1;
-        ArrayList<Block> t2;
-        long encodeCpuTimeStart = getCpuTime();
-        t1 = suf1.encode(S);
-        long encodeCpuTimeEnd = getCpuTime() - encodeCpuTimeStart;
-        long encodeTimeStart = System.nanoTime();
-        t2 = suf2.encode(S);
-        long encodeTimeEnd = System.nanoTime() - encodeTimeStart;
-
-        long decodeCpuTimeStart = getCpuTime();
-        suf1.decode(t1);
-        long decodeCpuTimeEnd = getCpuTime() - decodeCpuTimeStart;
-        long decodeTimeStart = System.nanoTime();
-        suf2.decode(t2);
-        long decodeTimeEnd = System.nanoTime() - decodeTimeStart;
+            System.out.println("Encoding the string..");
+            ArrayList<Block> t1;
+            ArrayList<Block> t2;
+            long encodeCpuTimeStart = getCpuTime();
+            t1 = suf1.encode(S);
+            long encodeCpuTimeEnd = getCpuTime() - encodeCpuTimeStart;
+            long encodeTimeStart = System.nanoTime();
+            t2 = suf2.encode(S);
+            long encodeTimeEnd = System.nanoTime() - encodeTimeStart;
 
 
-        System.out.println("WALLCLOCK TIMES");
-        System.out.println("Suffix tree construction: " + suffixTreeTimeEnd/(1000000000.0));
-        System.out.println("encode: " + encodeTimeEnd/(1000000000.0));
-        System.out.println("decode: " + decodeTimeEnd/(1000000000.0));
+            System.out.println("Decoding the string..");
+            long decodeCpuTimeStart = getCpuTime();
+            suf1.decode(t1);
+            long decodeCpuTimeEnd = getCpuTime() - decodeCpuTimeStart;
+            long decodeTimeStart = System.nanoTime();
+            suf2.decode(t2);
+            long decodeTimeEnd = System.nanoTime() - decodeTimeStart;
 
-        System.out.println();
+            System.out.println("Done!..");
 
-        System.out.println("CPU TIMES");
-        System.out.println("Suffix tree construction: " + suffixTreeCpuTimeEnd/(1000000000.0));
-        System.out.println("encode: " + encodeCpuTimeEnd/(1000000000.0));
-        System.out.println("decode: " + decodeCpuTimeEnd/(1000000000.0));
+            System.out.println("WALLCLOCK TIMES");
+            System.out.println("Suffix tree construction: " + suffixTreeTimeEnd/(1000000000.0));
+            System.out.println("encode: " + encodeTimeEnd/(1000000000.0));
+            System.out.println("decode: " + decodeTimeEnd/(1000000000.0));
+
+            System.out.println();
+
+            System.out.println("CPU TIMES");
+            System.out.println("Suffix tree construction: " + suffixTreeCpuTimeEnd/(1000000000.0));
+            System.out.println("encode: " + encodeCpuTimeEnd/(1000000000.0));
+            System.out.println("decode: " + decodeCpuTimeEnd/(1000000000.0));
 
         } catch (IOException e) {
             e.printStackTrace();
