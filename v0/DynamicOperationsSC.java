d4f6f363c45a664efa502b89db0bca65630a7e13
josefinetusindfryd <josefinetusindfryd@gmail.com>
2016-04-19 12:16:25 +0200

    Nu med KMP algo tilføjet ;)

---
+++ v0/DynamicOperationsSC.java
@@ -0,0 +1,300 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Josefinetusindfryd on 18/04/16.
+ */
+public class DynamicOperationsSC extends DynamicOperations {
+
+    BinaryTree dps;
+    KMPAlgo KMP;
+
+    //constructor
+    public DynamicOperationsSC(ArrayList<Block> C, Compressor cmp) {
+        super(C, cmp);
+        this.dps = new BinaryTree(C);
+        this.KMP = new KMPAlgo();
+    }
+
+    //getter for C - bruges i test
+    public ArrayList<Block> getC() {
+        return this.C;
+    }
+
+    //getter for dps - bruges i test
+    public BinaryTree getDPS() {
+        return this.dps;
+    }
+
+
+    // Return character S[i]
+    // Index out of bound exception
+    public char access(int index) throws IndexOutOfBoundsException {
+
+        //get all positions and distances
+        int[] BS = this.dps.searchReturnIndexSIR(index);
+
+        int nodeIndex = BS[0]; //BS[0] index of the node/block in which the index is contained
+        int startPosInR = BS[1]; //start position in R of the node/block containing index
+
+        int startPosInS;
+        if (nodeIndex == 0) startPosInS = 0;
+        else startPosInS = this.dps.sum(nodeIndex - 1);
+
+        return this.cmp.RA[startPosInR + (index - startPosInS)];
+    }
+
+    public void replace(int index, char sub) {
+
+        // O(|R|)
+        int indexOfReplacingCharInR = this.cmp.indexOf(new char[]{sub});
+
+
+        //get all positions and distances
+        // O(logn)
+        int[] BS = this.dps.searchReturnIndexSIR(index);
+
+        int nodeIndex = BS[0]; //index of the node/block in which the index is contained
+        int startPosInR = BS[1]; //start position in R of the node/block containing index
+        int length = BS[2]; //length of the node/block containing index
+
+        int startPosInS; //start position in S of the node/block containing index
+        if (nodeIndex == 0) startPosInS = 0;
+            //O(logn)
+        else startPosInS = this.dps.sum(nodeIndex - 1);
+
+        //offset in Ref string
+        int offsetInR = (index - startPosInS);
+
+        //return if the char to be is replaced is the same as the one substituting it.
+        char charToReplace = this.cmp.RA[startPosInR + (index - startPosInS)];
+        if (charToReplace == sub) return;
+
+
+        //case 1
+        //when block can be split in 3, i is in the middle
+        //i not first in block, i not the last in block, length is 3 or more
+        if (offsetInR > 0 && offsetInR != (length - 1) && length >= 3) {
+
+            int divValue;
+            if (offsetInR == 1) divValue = 1;
+            else divValue = offsetInR;
+
+
+            this.dps.divide(nodeIndex, divValue);
+            this.dps.divide(nodeIndex + 1, 1);
+            this.dps.updateSIR(nodeIndex + 1, indexOfReplacingCharInR);
+
+        }
+        //case 2
+        //i is the first in the block and length is at least 2
+        else if (offsetInR == 0 && length >= 2) {
+
+            this.dps.divide(nodeIndex, 1);
+            this.dps.updateSIR(nodeIndex, indexOfReplacingCharInR);
+
+        }
+        //case 3
+        //i is the last index and length is at least 2
+        else if (offsetInR == (length - 1) && length >= 2) {
+
+            this.dps.divide(nodeIndex, length - 1);
+            this.dps.updateSIR(nodeIndex + 1, indexOfReplacingCharInR);
+
+        }
+        //case 4
+        //replace with a single char
+        else if (length == 1) {
+
+            this.dps.updateSIR(nodeIndex, indexOfReplacingCharInR);
+
+        } else {
+            throw new IllegalArgumentException("Case not covered");
+        }
+
+        restoreMax((Math.max(nodeIndex - 1, 0)), (Math.min(nodeIndex + 4, (this.dps.getTotalLeafs() - 1))));
+
+
+    }
+
+    public void insert(int index, char c) {
+
+
+        int indexOfR = this.cmp.indexOf(new char[]{c});
+        if (indexOfR == -1) throw new IllegalArgumentException("character is not in the reference string");
+
+        // insert at the end of string
+        if (index == super.getSLength()) {
+            this.dps.insert(this.dps.getTotalLeafs(), 1, indexOfR);
+
+            restoreMax((this.dps.getTotalLeafs() - 2), (this.dps.getTotalLeafs() - 1));
+            return;
+        }
+
+        //get all positions and distances
+        int[] BS = this.dps.searchReturnIndexSIR(index);
+        int startPosInR = BS[1]; //B[1] is the starting position of block BS[0] in R
+        int nodeIndex = BS[0]; //BS[0] is index of the node holding the index
+        int startPosInS = (nodeIndex == 0) ? 0 : this.dps.sum(nodeIndex - 1);
+        int length = BS[2]; // B[2] is the length of the node holding the index
+        int offSet = index - startPosInS;
+        //int offsetInR = startPosInR + (index - startPosInS);
+
+        //insert in beginning of block -> insert new node before the found node
+        // also covers case, where length of node is only 1
+        if (index == startPosInS) {
+
+            this.dps.insert(nodeIndex, 1, indexOfR);
+
+        } else {//middle of block
+            // also covers if the character is inserted at the last index of the block - since the old character here will then be z
+
+            // divide node xyz ---- x   yz
+            // y is the index at which the new node must be inserted - so by inserting a new node after x the new node get the correct index
+            this.dps.divide(nodeIndex, offSet);
+            this.dps.insert(nodeIndex + 1, 1, indexOfR);
+        }
+
+        restoreMax((Math.max(nodeIndex - 1, 0)), (Math.min(nodeIndex + 4, (this.dps.getTotalLeafs() - 1))));
+    }
+
+    public void delete(int index) {
+
+        //get all positions and distances
+        int[] BS = this.dps.searchReturnIndexSIR(index);
+        int startPosInR = BS[1]; //B[1] is the starting position of node BS[0] in R
+        int nodeIndex = BS[0]; //BS[0] is index of the node holding the index
+        int startPosInS = (nodeIndex == 0) ? 0 : this.dps.sum(nodeIndex - 1);
+        int length = BS[2]; // B[2] is the length of the node holding the index
+        int offSet = index - startPosInS;
+
+        //case 1
+        //when node can be split in 3, i is in the middle
+        //i not first in node, i not the last in node, length is 3 or more
+        if (offSet > 0 && offSet != (length - 1) && length >= 3) {
+
+            // divide det node xyz -> x   y   z
+            // y is the node of length 1 containing the relevant index
+            this.dps.divide(nodeIndex, offSet);
+            this.dps.divide(nodeIndex + 1, 1);
+            this.dps.delete(nodeIndex + 1);
+        }
+        //case 2
+        //i is the first in the node and length is at least 2
+        else if (offSet == 0 && length >= 2) {
+
+            // divide det node yz ->   y   z
+            // y is the node of length 1 containing the relevant index
+            this.dps.divide(nodeIndex, 1);
+            this.dps.delete(nodeIndex);
+
+        }
+        //case 3
+        //i is the last index and length is at least 2
+        else if (offSet == (length - 1) && length >= 2) {
+
+            // divide det node xy -> x   y
+            // y is the node of length 1 containing the relevant index
+            this.dps.divide(nodeIndex, offSet);
+            this.dps.delete(nodeIndex + 1);
+
+        }
+        //case 4
+        //i is in a node with length 1
+        else if (length == 1) {
+            this.dps.delete(nodeIndex);
+
+        } else {
+            throw new IllegalArgumentException("Case not covered");
+        }
+
+        restoreMax((Math.max(nodeIndex - 1, 0)), (Math.min(nodeIndex + 4, (this.dps.getTotalLeafs() - 1))));
+
+    }
+
+    private void restoreMax(int startNode, int endNode) {
+        this.dps.prettyPrintBinary(this.dps.getRoot(), 0);
+
+        int index = -1;
+        char[] a;
+        char[] b;
+        int[] aBS;
+        int[] bBS;
+
+        for (int i = startNode; i < endNode; i++) {
+            aBS = this.dps.find(i);
+            bBS = this.dps.find(i + 1);
+            a = new char[aBS[1]];
+            b = new char[bBS[1]];
+            System.arraycopy(this.cmp.RA, aBS[0], a, 0, aBS[1]);
+            System.arraycopy(this.cmp.RA, bBS[0], b, 0, bBS[1]);
+
+            index = this.KMP.searchSubStringS(this.cmp.RA,a, b);
+
+            if (index > -1) {// yes can be merged.
+                this.dps.merge(i);
+                this.dps.updateSIR(i, index);
+                endNode--; //  endNode must be decremented (since there is now one less node to look at)
+                i--; // stay at same i value - must try to merge with the following node
+                continue;
+
+            }
+
+        }
+    }
+
+
+
+
+/*
+        StringBuilder rest = new StringBuilder();
+        int index = -1;
+        int[] aBS;
+        int[] bBS;
+        char[] a;
+        for (int i = startNode; i < endNode - 1; i++) {
+            // the current node could not be merged with the earlier one - rest is now empty
+            // so look at the two next nodes
+            if (rest.length() == 0) {
+                aBS = this.dps.find(i);
+                a = Arrays.copyOfRange(this.cmp.RA, aBS[0], aBS[0] + aBS[1]);
+                bBS = this.dps.find(i + 1);
+                char[] b = Arrays.copyOfRange(this.cmp.RA, bBS[0], bBS[0] + bBS[1]);
+                rest.append(a);
+                rest.append(b);
+                index = this.cmp.indexOf(rest.toString().toCharArray());
+
+                if (index > -1) {// yes can be merged. endNode must be decremented (since there is now one less node to look at)
+                    this.dps.merge(i);
+                    this.dps.updateSIR(i, index);
+                    endNode--;
+                    //this.C.get(i).setLength(rest.length());
+                    //this.C.get(i).setPos(index);
+                    //this.C.remove(i + 1);
+                    continue;
+                }
+            } else { // The nodes before could be merged (is now at index i-1) - test if the current node can also be merged with these
+                // again endNode must be decremented (since there is now one less node to look at)
+                aBS = this.dps.find(i);
+                a = Arrays.copyOfRange(this.cmp.RA, aBS[0], aBS[0] + aBS[1]);
+                rest.append(a);
+                index = this.cmp.indexOf(rest.toString().toCharArray());
+                if (index > -1) {
+                    this.dps.merge(i - 1);
+                    this.dps.updateSIR(i-1, index);
+                    i --;
+                    endNode --;
+                    //this.C.get(i - 1).setLength(rest.length());
+                    //this.C.get(i - 1).setPos(index);
+                    //this.C.remove(i);
+                    continue;
+                }
+                // must look at the current node once more with the consecutive node
+                i --;
+            }
+            // if the nodes could not be merges (no continue is reached) the rest must be emptied before moving on to next node
+            rest.setLength(0);
+
+        }*/
+
+}
---
+++ v0/KMPAlgo.java
@@ -0,0 +1,134 @@
+/**
+ * Created by Josefinetusindfryd on 14/04/16.
+ */
+//package com.forum.java.algos;
+
+public class KMPAlgo {
+
+    /**
+     * Pre processes the pattern array based on proper prefixes and proper
+     * suffixes at every position of the array
+     *
+     * @param ptrn
+     *            word that is to be searched in the search string
+     * @return partial match table which indicates
+     */
+    public int[] preProcessPattern(char[] ptrn) {
+        int i = 0, j = -1;
+        int ptrnLen = ptrn.length;
+        int[] b = new int[ptrnLen + 1];
+
+        b[i] = j;
+        while (i < ptrnLen) {
+            while (j >= 0 && ptrn[i] != ptrn[j]) {
+                // if there is mismatch consider next widest border
+                j = b[j];
+            }
+            i++;
+            j++;
+            b[i] = j;
+        }
+        /*
+        // print pettern, partial match table and index
+        System.out
+                .println("printing pattern, partial match table, and its index");
+        System.out.print(" ");
+        for (char c : ptrn) {
+            System.out.print(c + "   ");
+        }
+        System.out.println(" ");
+        for (int tmp : b) {
+            System.out.print(tmp + "   ");
+        }
+        System.out.print("\n ");
+        for (int l = 0; l < ptrn.length; l++) {
+            System.out.print(l + "   ");
+        }
+        System.out.println();
+        */
+        return b;
+    }
+
+    /**
+     * Based on the pre processed array, search for the pattern in the text
+     *
+     * @param text
+     *            text over which search happens
+     * @param ptrn
+     *            pattern that is to be searched
+     */
+    public void searchSubString(char[] text, char[] ptrn) {
+        int i = 0, j = 0;
+        // pattern and text lengths
+        int ptrnLen = ptrn.length;
+        int txtLen = text.length;
+
+        // initialize new array and preprocess the pattern
+        int[] b = preProcessPattern(ptrn);
+
+        while (i < txtLen) {
+            while (j >= 0 && text[i] != ptrn[j]) {
+                /*System.out.println("Mismatch happened, between text char "
+                        + text[i] + " and pattern char " + ptrn[j]
+                        + ", \nhence jumping the value of " + "j from " + j
+                        + " to " + b[j] + " at text index i at " + i
+                        + " based on partial match table");*/
+                j = b[j];
+            }
+            i++;
+            j++;
+
+            // a match is found
+            if (j == ptrnLen) {
+                System.out.println("FOUND SUBSTRING AT i " + i + " and index:"
+                        + (i - ptrnLen));
+                //System.out.println("Setting j from " + j + " to " + b[j]);
+                return;
+               // j = b[j];
+            }
+        }
+    }
+
+    // Tanke to
+    public int searchSubStringS(char[] text, char[] ptrn1, char[] ptrn2) {
+        int i = 0, j = 0;
+
+        int Len1 = ptrn1.length;
+        int Len2 = ptrn2.length;
+        char[] ptrn = new char[Len1+Len2];
+        System.arraycopy(ptrn1, 0, ptrn, 0, Len1);
+        System.arraycopy(ptrn2, 0, ptrn, Len1, Len2);
+
+        //System.out.println(ptrn);
+
+        // pattern and text lengths
+        int ptrnLen = ptrn.length;
+        int txtLen = text.length;
+
+        // initialize new array and preprocess the pattern
+        int[] b = preProcessPattern(ptrn);
+
+        while (i < txtLen) {
+            while (j >= 0 && text[i] != ptrn[j]) {
+                /*System.out.println("Mismatch happened, between text char "
+                        + text[i] + " and pattern char " + ptrn[j]
+                        + ", \nhence jumping the value of " + "j from " + j
+                        + " to " + b[j] + " at text index i at " + i
+                        + " based on partial match table");*/
+                j = b[j];
+            }
+            i++;
+            j++;
+
+            // a match is found
+            if (j == ptrnLen) {
+                //System.out.println("FOUND SUBSTRING AT i " + i + " and index:"
+                //        + (i - ptrnLen));
+                //System.out.println("Setting j from " + j + " to " + b[j]);
+                return (i - ptrnLen);
+                // j = b[j];
+            }
+        }
+        return -1;
+    }
+}
